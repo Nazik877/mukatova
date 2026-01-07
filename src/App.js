373dd48aa850f9235383453928c275beddb27b6c
Nazik877 <mukatovanazik75@gmail.com>
2026-01-07 16:10:52 +0600

    cread companent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-
+import Sim from './companent/Sim';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+    <Sim/>
     </div>
   );
 }
---
+++ src/companent/Sim.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Sim = () => {
+  return (
+    <div>
+      <h1>
+        Hello world
+        <p>
+            привет
+        </p>
+      </h1>
+    </div>
+  )
+}
+
+export default Sim
